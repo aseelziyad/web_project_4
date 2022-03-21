16e98fb3f96ce42ed0ec9ac21d62842480828c31
aseel ziyad <aseeelziyad@gmail.com>
2022-03-21 21:52:03 +0200

    project 9

--- src/components/PopupWithSubmit.js
+++ src/components/PopupWithSubmit.js
@@ -1,21 +1,20 @@
 import Popup from "./Popup.js";
 
 export default class PopupWithSubmit extends Popup {
-    constructor(popupSelector) {
-        super(popupSelector);
-        this._form = this._popup.querySelector(".popup__form");
-    }
+  constructor(popupSelector) {
+    super(popupSelector);
+    this._form = this._popup.querySelector(".popup__form");
+  }
 
-    setAction(action) {
-        this._handleSubmit = action;
-    }
+  setAction(action) {
+    this._handleSubmit = action;
+  }
 
-
-    setEventListeners() {
-            super.setEventListeners();
-        this._popup.addEventListener("submit", (evt) => {
-            evt.preventDefault();
-              this._handleSubmit();
-            });
-          }
-    }
+  setEventListeners() {
+    super.setEventListeners();
+    this._popup.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+      this._handleSubmit();
+    });
+  }
+}
